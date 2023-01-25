85984ec9e42d36aacabec38ec94d5e2db3e78a72
chagum26 <lievrehugo26230@gmail.com>
2023-01-25 23:17:10 +0100

    continuation page

--- chagum/src/app/shared/primeng.module.ts
+++ chagum/src/app/shared/primeng.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
   declarations: [],
-  imports: [
+  imports: [],
+  exports: [
+    SidebarModule,
+    ButtonModule,
   ]
 })
 export class PrimengModule { }
